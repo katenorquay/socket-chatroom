e455c352846793f8901c474b3d3850870cb6c124
katenorquay <katenorquay@live.com>
2017-02-13 13:21:48 +1300

    refactored showmessage into seperate component

--- client/components/App.js
+++ client/components/App.js
@@ -8,27 +8,11 @@ var EnterMessage = require('./EnterMessage')
 var ShowMessage = require('./ShowMessage')
 
 function App({state, dispatch}) {
-
-  var messageClass = state.loggedin === false ? 'hide': 'chatlog'
-
   return (
     <div>
       <h1>Socket Chat</h1>
       <SignIn state={state} dispatch={dispatch} />
-      <div className={messageClass}>
-        {state.messages.map((message) => {
-          var redClass = message.color === 'red' ? 'red' : ''
-          var blueClass = message.color === 'blue' ? 'blue' : ''
-          var greenClass = message.color === 'green' ? 'green' : ''
-          var yellowClass = message.color === 'yellow' ? 'yellow' : ''
-          return (
-            <div>
-              <p className='username'>{message.user}:</p>
-              <p className={redClass + blueClass + greenClass + yellowClass}>{message.text}</p>
-            </div>
-          )  
-          })}
-      </div>
+      <ShowMessage state={state} dispatch={dispatch} />
       <EnterMessage state={state} dispatch={dispatch} />
     </div>
   )
--- client/components/ShowMessage.js
+++ client/components/ShowMessage.js
@@ -1,19 +1,26 @@
 var React = require('react')
 var render = require('react-dom').render
 
-
-  var messageClass = state.loggedin === false ? 'hide': 'chatlog'
-<div className={messageClass}>
-  {state.messages.map((message) => {
-    var redClass = message.color === 'red' ? 'red' : ''
-    var blueClass = message.color === 'blue' ? 'blue' : ''
-    var greenClass = message.color === 'green' ? 'green' : ''
-    var yellowClass = message.color === 'yellow' ? 'yellow' : ''
+function ShowMessage({state, dispatch}) {
+  if(state.loggedin) {
     return (
-      <div>
-        <p className='username'>{message.user}:</p>
-        <p className={redClass + blueClass + greenClass + yellowClass}>{message.text}</p>
+      <div className='chatlog'>
+        {state.messages.map((message) => {
+          var redClass = message.color === 'red' ? 'red' : ''
+          var blueClass = message.color === 'blue' ? 'blue' : ''
+          var greenClass = message.color === 'green' ? 'green' : ''
+          var yellowClass = message.color === 'yellow' ? 'yellow' : ''
+          return (
+            <div>
+              <p className='username'>{message.user}:</p>
+              <p className={redClass + blueClass + greenClass + yellowClass}>{message.text}</p>
+              </div>
+          )
+        })}
       </div>
     )
-    })}
-</div>
+  }
+}
+
+
+module.exports = ShowMessage
